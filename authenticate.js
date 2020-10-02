386a0b8b097c39d4fa208aae8e63525750e6bc5d
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-10-02 17:07:09 -0400

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -4,24 +4,15 @@ const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 const config = require('./config.js');
-
-exports.verifyAdmin = function (req, next) {
-    if (req.user.admin) {
-        return next();
-    } else {
-        const err = new Error(`You are not authorized to perform this operation!`)
-        err.status = 403;
-        return next(err);
-    }
-}
 
     exports.local = passport.use(new LocalStrategy(User.authenticate()));
     passport.serializeUser(User.serializeUser());
     passport.deserializeUser(User.deserializeUser());
 
-    exports.getToken = function (user) {
+    exports.getToken = user => {
         return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
     };
 
@@ -48,3 +39,44 @@ exports.verifyAdmin = function (req, next) {
     );
 
     exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+    exports.verifyAdmin = function (req, res, next) {
+        if (req.user.admin) {
+            return next();
+        } else {
+            const err = new Error(`You are not authorized to perform this operation!`)
+            err.status = 403;
+            return next(err);
+        }
+    }
+
+    exports.facebookPassport = passport.use(
+        new FacebookTokenStrategy(
+            {
+                clientID: config.facebook.clientId,
+                clientSecret: config.facebook.clientSecret
+            }, 
+            (accessToken, refreshToken, profile, done) => {
+                User.findOne({facebookId: profile.id}, (err, user) => {
+                    if (err) {
+                        return done(err, false);
+                    }
+                    if (!err && user) {
+                        return done(null, user);
+                    } else {
+                        user = new User({ username: profile.displayName });
+                        user.facebookId = profile.id;
+                        user.firstname = profile.name.givenName;
+                        user.lastname = profile.name.familyName;
+                        user.save((err, user) => {
+                            if (err) {
+                                return done(err, false);
+                            } else {
+                                return done(null, user);
+                            }
+                        });
+                    }
+                });
+            }
+        )
+    );
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '765026830730849',
+        clientSecret: '5fda5c40534fe1844f738f1b67ef9257'
+    }
 }
